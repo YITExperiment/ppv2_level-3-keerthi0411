843951df972615ea49ae17688f9ba35d54f8554f
keerthi0411 <134826563+keerthi0411@users.noreply.github.com>
2023-05-30 12:22:34 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,13 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=15
+>>> print(age)
+15
+>>> 
+>>> weight=43
+>>> print(weight)
+43
+>>> 
+>>> height=167
+>>> print(height)
+167
